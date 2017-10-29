9442160878469d97ae992fd2b003539b1a1c4889
UmangChamaria <umangchamaria@gmail.com>
2017-10-29 18:46:56 +0530

    extending AppCompatAutoComplete gradle version update manifest update

--- extended-auto-complete/src/main/java/com/umang/extendedautocomplete/ExtendedAutoCompleteTextView.java
+++ extended-auto-complete/src/main/java/com/umang/extendedautocomplete/ExtendedAutoCompleteTextView.java
@@ -1,18 +1,14 @@
 package com.umang.extendedautocomplete;
 
-import android.annotation.TargetApi;
 import android.content.Context;
-import android.content.res.Resources.Theme;
-import android.os.Build.VERSION_CODES;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
-import android.widget.AutoCompleteTextView;
 
 /**
  * @author Umang Chamaria
  */
-
-public class ExtendedAutoCompleteTextView extends AutoCompleteTextView {
+public class ExtendedAutoCompleteTextView
+    extends android.support.v7.widget.AppCompatAutoCompleteTextView{
   public ExtendedAutoCompleteTextView(Context context) {
     super(context);
   }
@@ -25,28 +21,13 @@ public class ExtendedAutoCompleteTextView extends AutoCompleteTextView {
     super(context, attrs, defStyleAttr);
   }
 
-  @TargetApi(VERSION_CODES.LOLLIPOP)
-  public ExtendedAutoCompleteTextView(Context context, AttributeSet attrs, int defStyleAttr,
-      int defStyleRes) {
-    super(context, attrs, defStyleAttr, defStyleRes);
-  }
-
-  @TargetApi(VERSION_CODES.N)
-  public ExtendedAutoCompleteTextView(Context context, AttributeSet attrs, int defStyleAttr,
-      int defStyleRes, Theme popupTheme) {
-    super(context, attrs, defStyleAttr, defStyleRes, popupTheme);
-  }
-
-  @Override
-  public boolean onTouchEvent(MotionEvent event) {
+  @Override public boolean onTouchEvent(MotionEvent event) {
     setDropDownBackgroundResource(R.color.milky_white);
     showDropDown();
     return super.onTouchEvent(event);
   }
 
-
-  @Override
-  public boolean enoughToFilter() {
+  @Override public boolean enoughToFilter() {
     return true;
   }
 }
